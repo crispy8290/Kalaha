cb0cb644d605cd9891bceeb432d56d764f934613
gilsonn <gilsonnyirimana@gmail.com>
2022-08-25 19:59:36 -0400

    + custom java formatter, + Player and - HelloWorld

---
+++ src/Player.java
@@ -0,0 +1,32 @@
+public class Player
+{
+	private String playerName;
+	private int playerWinCounter;
+
+	// Constructor, pn is an alias for playerName
+	Player(String pn)
+	{
+		this.playerName = pn;
+		this.playerWinCounter = 0;
+	}
+
+	// Mutator method
+	public String getPlayerName()
+	{
+		return playerName;
+	}
+
+	// Mutator method
+	public int getPlayerWinCounter()
+	{
+		return playerWinCounter;
+	}
+
+	// Mutator method; pwc is an alias for playerWinCounter
+	public void setPlayerWinCounter(int pwc)
+	{
+		this.playerWinCounter = pwc;
+	}
+}
+
+// Mess around with the access modifiers when you get a chance.
