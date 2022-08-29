f6effa530ef009efa67f60a1e43b24b5b21533dd
gilssonn <gilsonnyirimana@gmail.com>
2022-08-28 23:42:56 -0400

    Updated Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,25 @@
-public class Player 
-{
-	
+public class Player {
+    private String playerName;
+    private int playerWinCounter;
+
+    // Constructor, pn is an alias for playerName
+    Player(String pn) {
+        this.playerName = pn;
+        this.playerWinCounter = 0;
+    }
+
+    // Mutator method
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    // Mutator method
+    public int getPlayerWinCounter() {
+        return playerWinCounter;
+    }
+
+    // Mutator method; pwc is an alias for playerWinCounter
+    public void setPlayerWinCounter(int pwc) {
+        this.playerWinCounter = pwc;
+    }
 }
